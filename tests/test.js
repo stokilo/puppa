b41df6b7e3d9637ba5a92d5fac04c8d02cdc8c47
slawomirstec <consol>
2017-08-26 13:26:21 +0400

    Remove github test, update google test.

--- tests/test.js
+++ tests/test.js
@@ -1,21 +1,7 @@
 testGoogle = async function (config) {
-    // change to english (not required)
-    // await waitFor(() => elementByContent('a', 'English')).then(result => result[0].click());
     // enter term 'test'
-    await waitFor("#lst-ib").then(result => result.val('test'));
+    await waitFor(() => $("#contentId").contents().find("#lst-ib")).then(result => result.val('test'));
     // search
-    await waitFor("#tsf").then(result => result[0].submit());
+    await waitFor(() => $("#contentId").contents().find("#tsf")).then(result => result[0].submit());
 }
 
- testGithub = async function (config) {
-    // enter 'test' into Github search on the main page 
-    await waitFor("input[name=q]").then(result => result.val("test")); 
-    // submit the form
-    //await waitFor(".js-site-search-form").then(result => result.submit());
-    // TODO: new page after submit, it would require injecting again all js files including test !
-    // TODO: what is more important, this javascript execution is terminated after submit, this throws an error and 
-    // TODO: interrup test
-    // click on Java tag on the project list
-    //window.aaa = await function(){ alert('a'); };
-    //await waitFor(() => elementByContent('a', 'Java')).then(result => result.click());
-}
