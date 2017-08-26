2e77d0dc19766c79c61569f2952de52220c83d47
slawomirstec <consol>
2017-08-26 13:17:01 +0400

    Strip more headers, add sandbox to iframe.

--- tests/test.js
+++ tests/test.js
@@ -11,12 +11,11 @@ testGoogle = async function (config) {
     // enter 'test' into Github search on the main page 
     await waitFor("input[name=q]").then(result => result.val("test")); 
     // submit the form
-    window.aaa = await function(){ alert('a'); };
-    await waitFor(".js-site-search-form").then(result => result.submit());
+    //await waitFor(".js-site-search-form").then(result => result.submit());
     // TODO: new page after submit, it would require injecting again all js files including test !
     // TODO: what is more important, this javascript execution is terminated after submit, this throws an error and 
     // TODO: interrup test
     // click on Java tag on the project list
-    window.aaa = await function(){ alert('a'); };
+    //window.aaa = await function(){ alert('a'); };
     //await waitFor(() => elementByContent('a', 'Java')).then(result => result.click());
 }
