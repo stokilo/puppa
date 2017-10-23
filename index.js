14468e3c7730e30d6dd4392e050277d0f4eda5eb
Slawomir Stec <st@consol.ae>
2017-10-23 08:39:16 +0400

    Add node module.

--- index.js
+++ index.js
@@ -7,62 +7,66 @@ const configParser = require("./modules/config.js");
 const runner = require("./modules/runner.js");
 const Table = require('cli-table');
 
-// process user test run command 
-var commandResult = cli.processCommand(__dirname);
-cli.validateProcessCommandResult(commandResult);
+module.exports.run = function () {
 
-// load configuration for defined test suite and merge with the selected user profile settings
-var testConfiguration = configParser.parseConfiguration(__dirname, commandResult);
-configParser.validateTestConfiguration(testConfiguration);
+	// process user test run command 
+	var commandResult = cli.processCommand(__dirname);
+	cli.validateProcessCommandResult(commandResult);
 
-// extract final configuration into variable to be used by test runner on multiple tabs
-var config = testConfiguration.configuration;
-const testCases = config.testSuite.order;
+	// load configuration for defined test suite and merge with the selected user profile settings
+	var testConfiguration = configParser.parseConfiguration(__dirname, commandResult);
+	configParser.validateTestConfiguration(testConfiguration);
+
+	// extract final configuration into variable to be used by test runner on multiple tabs
+	var config = testConfiguration.configuration;
+	const testCases = config.testSuite.order;
 
 
-// test case summary
-var summary = new Table({
-	head: ['Test name', 'Result', 'Error details']
-});
-
-(async () => {
-
-	// note: headless chrome won"t support extensions so tests against protected sites require headless:false
-	const browser = await puppeteer.launch({
-		args: [
-			"--disable-web-security",
-			"--load-extension=" + __dirname + path.sep + "lib" + path.sep + "ignore-headers",
-			"--no-first-run"],
-		headless: config.browserConfig.headless,
-		dumpio: config.browserConfig.dumpio
+	// test case summary
+	var summary = new Table({
+		head: ['Test name', 'Result', 'Error details']
 	});
 
-	// run tests on each tab ...
-	var promises = [];
-	var batchResults = [];
-	for (var testCase in testCases) {
-		promises.push(
-			runner.runTests(__dirname, browser, testCases[testCase], config).then(
-				(batchResult) => batchResults= batchResults.concat(batchResult)
-			)
-		);
-	}
+	(async () => {
 
-	// ... and wait until all finish execution
-	await Promise.all(promises);
-	if (config.browserConfig.closeBrowser) {
-		await browser.close();
-	}
+		// note: headless chrome won"t support extensions so tests against protected sites require headless:false
+		const browser = await puppeteer.launch({
+			args: [
+				"--disable-web-security",
+				"--load-extension=" + __dirname + path.sep + "lib" + path.sep + "ignore-headers",
+				"--no-first-run"],
+			headless: config.browserConfig.headless,
+			dumpio: config.browserConfig.dumpio
+		});
 
-	// create final test result summary
-	for (var j = 0; j < batchResults.length; j++) {
-		var singleResult = batchResults[j];
-		summary.push(
-			[singleResult.testName,
-			singleResult.passed ? colors.green("PASSED") : colors.red("FAILED"),
-			!singleResult.passed ? singleResult.error : ""
-			]
-		);
-	}
-	console.log(summary.toString());
-})();
+		// run tests on each tab ...
+		var promises = [];
+		var batchResults = [];
+		for (var testCase in testCases) {
+			promises.push(
+				runner.runTests(__dirname, browser, testCases[testCase], config).then(
+					(batchResult) => batchResults = batchResults.concat(batchResult)
+				)
+			);
+		}
+
+		// ... and wait until all finish execution
+		await Promise.all(promises);
+		if (config.browserConfig.closeBrowser) {
+			await browser.close();
+		}
+
+		// create final test result summary
+		for (var j = 0; j < batchResults.length; j++) {
+			var singleResult = batchResults[j];
+			summary.push(
+				[singleResult.testName,
+				singleResult.passed ? colors.green("PASSED") : colors.red("FAILED"),
+				!singleResult.passed ? singleResult.error : ""
+				]
+			);
+		}
+		console.log(summary.toString());
+	})();
+
+};
